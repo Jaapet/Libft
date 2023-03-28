e448ef974e29bdf4096d37e8f381c6d522b557e8
Noé Desprez <63704377+Jaapet@users.noreply.github.com>
2023-03-29 01:07:15 +0200

    strchr + strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndesprez <ndesprez@student.42nice.f>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 01:03:08 by ndesprez          #+#    #+#             */
+/*   Updated: 2023/03/29 01:03:08 by ndesprez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strrchr(const char *s, int c)
+{
+    int i;
+    int cpt;
+
+    i = 0;
+    cpt = 0;
+    while (s[i])
+    {
+        if (s[i] == (unsigned char)c) {
+            cpt++;
+            if (cpt == 2)
+                return ((char *) (s + i));
+        }
+        i++;
+    }
+    return (NULL);
+}
