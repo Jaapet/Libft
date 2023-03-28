1c6377c5996afc015272ea6168438bb347124480
Noé Desprez <63704377+Jaapet@users.noreply.github.com>
2023-03-29 00:53:39 +0200

    toupper + tolower

--- libft.h
+++ libft.h
@@ -27,5 +27,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
 
 #endif
