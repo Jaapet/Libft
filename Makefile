70869f2d585f19e399d58d6ea7ec63708d04483b
ndesprez <ndesprez@student.42nice.fr>
2023-03-28 19:48:35 +0200

    strlcpy + begin strlcat

--- Makefile
+++ Makefile
@@ -6,11 +6,11 @@
 #    By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/27 14:43:36 by ndesprez          #+#    #+#              #
-#    Updated: 2023/03/28 17:42:31 by ndesprez         ###   ########.fr        #
+#    Updated: 2023/03/28 18:07:59 by ndesprez         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SOURCES	= ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c
+SOURCES	= ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c ft_strlcat.c
 NAME	= libft.a
 OBJECTS	=  ${SOURCES:.c=.o}
 .c.o	:
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,33 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/28 17:26:40 by ndesprez          #+#    #+#             */
-/*   Updated: 2023/03/28 18:04:10 by ndesprez         ###   ########.fr       */
+/*   Created: 2023/03/28 18:00:12 by ndesprez          #+#    #+#             */
+/*   Updated: 2023/03/28 19:46:38 by ndesprez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	srclen;
+	size_t	dstlen;
 	size_t	i;
-	int		cpt;
+	size_t	j;
 
-	cpt = 0;
-	while (src[cpt])
-		cpt++;
-	if (dstsize == 0)
-		return (cpt);
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
 	i = 0;
-	while (i < dstsize - 1 && src[i])
-	{
-		dst[i] = src[i];
+	j = 0;
+	while (dst[i] && i < dstsize)
 		i++;
-	}
-	dst[i] = '\0';
-	return (cpt);
+	while (src[j] && i < dstsize - 1)
+		dst[i++] = src[j++];
+	if (dstsize && dstlen <= dstsize)
+		dst[i] = '\0';
+	if (ft_strlen(dst) > dstsize)
+		return (srclen + dstsize);
+	else
+		return (srclen + dstlen);
 }
+
