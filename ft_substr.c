0e3be1385e5785fac8201be99f4feaa18b428df0
ndesprez <ndesprez@student.42nice.fr>
2023-03-30 18:35:38 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 12:32:44 by ndesprez          #+#    #+#             */
-/*   Updated: 2023/03/30 16:49:51 by ndesprez         ###   ########.fr       */
+/*   Updated: 2023/03/30 18:24:55 by ndesprez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*a;
 	size_t	i;
 
-	if (start >= ft_strlen(s))
-		return ("");
-	a = malloc(sizeof(char) * (len + 1));
+	if (start + len < ft_strlen(s))
+		a = malloc(sizeof(char) * (len + 1));
+	else if (start < ft_strlen(s))
+		a = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
+	else
+		a = malloc(sizeof(char));
 	if (!a)
 		return (NULL);
 	i = 0;
-	while (i < len && s[start + i])
+	if (start < ft_strlen(s))
 	{
-		a[i] = s[start + i];
-		i++;
+		while (i < len && s[start + i])
+		{
+			a[i] = s[start + i];
+			i++;
+		}
 	}
 	a[i] = '\0';
 	return (a);
